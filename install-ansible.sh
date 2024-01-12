d7529dc6f3710fb7f4c2f5288fa9c7051ddb8887
MannyFay <mannythefay@gmail.com>
2024-01-12 22:43:03 +0100

    Create Ansible installation script

---
+++ install-ansible.sh
@@ -0,0 +1,9 @@
+!#/usr/bin/env bash
+
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+
+
+
+# This script is for Linux. Don't now how to structure good the ansible repo by now.
