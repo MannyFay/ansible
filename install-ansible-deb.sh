e9795bb50a5eb626428543fba2cdecc1aa6edb1e
Manny Fay <mannythefay@gmail.com>
2024-01-21 10:31:52 +0100

    Renamed because on OS specific ansible installations

--- install-ansible-deb.sh
+++ install-ansible-deb.sh
@@ -4,6 +4,3 @@ sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 
-
-
-# This script is for Linux. Don't now how to structure good the ansible repo by now.
---
+++ install-ansible-mac.sh
@@ -0,0 +1,4 @@
+!#/usr/bin/env bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install ansible
