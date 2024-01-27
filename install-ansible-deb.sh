376a4d8f02d5b62ef8bc7beee9624606532baf0d
Manny Fay <mannythefay@gmail.com>
2024-01-27 09:44:03 +0100

    Implement upgrade block

--- install-ansible-deb.sh
+++ install-ansible-deb.sh
@@ -1,7 +1,14 @@
 !#/usr/bin/env bash
 
+sudo apt uptate && \
+sudo apt full-upgrade -y && \
+sudo apt autoremove -y && \
+sudo apt -f install -y && \
+sudo apt clean
+
 sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+
 sudo apt install git -y
 
